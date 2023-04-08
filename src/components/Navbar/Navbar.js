7cd32f9cd5495fa7219504cad6e33b2af55bf2e1
Aqil <aqilbayramli96@gmail.com>
2023-04-08 17:47:04 +0400

    V1.01

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Navbar.css';
+
+
+
+function Navbar() {
+  return (
+    <div>
+        <div>
+            <img className='poco__logo' src="https://demo.pocotheme.com/1/image/catalog/maza/svg/logo2.svg" alt="Poco theme" />
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
